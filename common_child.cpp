557ba06aa014219ae72d432d25fc6d3bd7a1be1b
palucki <piotrek.palucki@gmail.com>
2023-10-20 23:13:42 +0200

    Better solution using only indices

--- common_child.cpp
+++ common_child.cpp
@@ -4,21 +4,21 @@
 #include <algorithm>
 #include <deque>
 
-int lcs(std::string s1, std::string s2, int count)
+int lcs(const std::string& s1, int s1_idx, const std::string& s2, int s2_idx, int count)
 {
-    // std::cout << count << " handling " << s1 << " " << s2 << '\n';
-    if(s1.empty() || s2.empty())
+    if(s1_idx == -1 || s2_idx == -1)
     {
         return count;
     }
 
-    if(s1[s1.size() - 1] == s2[s2.size() - 1])
+    if(s1[s1_idx] == s2[s2_idx])
     {
-        // std::cout << "A MATCH!\n";
-        return lcs(s1.substr(0, s1.size() - 1), s2.substr(0, s2.size() - 1), count + 1);
+        return lcs(s1, s1_idx - 1, s2, s2_idx - 1, count + 1);
     }
 
-    count = std::max({count, lcs(s1.substr(0, s1.size() - 1), s2, count), lcs(s1, s2.substr(0, s2.size() - 1), count)});
+    count = std::max({count, 
+        lcs(s1, s1_idx - 1, s2, s2_idx, count), 
+        lcs(s1, s1_idx, s2, s2_idx - 1, count)}); 
     return count;
 }
 
@@ -26,7 +26,7 @@ int commonChild(std::string s1, std::string s2)
 {
     int result = 0;
 
-    result = lcs(s1, s2, 0);
+    result = lcs(s1, s1.size() - 1, s2, s2.size() - 1, 0);
 
     return result;
 }
