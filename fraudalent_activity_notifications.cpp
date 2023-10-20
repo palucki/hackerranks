6f4d8a3edd61e32ac13408e8d32094c08284891a
palucki <piotrek.palucki@gmail.com>
2023-10-20 22:16:11 +0200

    Used deque for performance reasons

--- fraudalent_activity_notifications.cpp
+++ fraudalent_activity_notifications.cpp
@@ -2,34 +2,44 @@
 #include <vector>
 #include <set>
 #include <algorithm>
+#include <deque>
 
 int activityNotifications(std::vector<int> expenditure, int d) 
 {
+    auto getMedian = [](const std::deque<int>& sorted_expenditure) {
+        
+        const int half = sorted_expenditure.size() / 2;
+        if(sorted_expenditure.size() % 2 == 0)
+        {
+            return (sorted_expenditure[half] + sorted_expenditure[half-1]) / 2.0;
+        }
+        else
+        {
+            return static_cast<double>(sorted_expenditure[half]);
+        }
+    };
+
     int notifications = 0;
-    
+    std::deque<int> sorted_expenditure{expenditure.begin(), expenditure.begin() + d};
+    std::sort(sorted_expenditure.begin(), sorted_expenditure.end());
+
     for(int i = d; i < expenditure.size(); ++i)
     {
-        std::vector<int> sorted_expenditure(expenditure.begin() + i - d, expenditure.begin() + i);
-        std::sort(sorted_expenditure.begin(), sorted_expenditure.end());
-        const double median = [&sorted_expenditure](){
-            const int half = sorted_expenditure.size() / 2;
-            
-            if(sorted_expenditure.size() % 2 == 0)
-            {
-                return (sorted_expenditure[half] + sorted_expenditure[half-1]) / 2.0;
-            }
-            else
-            {
-                return static_cast<double>(sorted_expenditure[half]);
-            }
-        }();
+        const int new_expense = expenditure[i];
+        const double median = getMedian(sorted_expenditure);
         
-        // std::cout << "Median is " << median << '\n';
-
-        if(expenditure[i] >= 2 * median)
+        if(new_expense >= 2 * median)
         {
             notifications++;
         }
+
+        // update sorted expenditures
+        int to_remove = expenditure[i - d];
+        auto to_remove_it = std::lower_bound(sorted_expenditure.begin(), sorted_expenditure.end(), to_remove);
+        sorted_expenditure.erase(to_remove_it);
+
+        auto insert_at = std::upper_bound(sorted_expenditure.begin(), sorted_expenditure.end(), new_expense);        
+        sorted_expenditure.insert(insert_at, new_expense);
     }
 
     return notifications;
