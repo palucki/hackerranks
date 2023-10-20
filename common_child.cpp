30c986e0f36414f00189bed59c9bdc61e282b82a
palucki <piotrek.palucki@gmail.com>
2023-10-21 00:51:45 +0200

    Used memoization to solve efficently

--- common_child.cpp
+++ common_child.cpp
@@ -4,29 +4,48 @@
 #include <algorithm>
 #include <deque>
 
-int lcs(const std::string& s1, int s1_idx, const std::string& s2, int s2_idx, int count)
+int lcs(const std::string& s1, int s1_idx, const std::string& s2, int s2_idx, std::vector<std::vector<int>>& results)
 {
     if(s1_idx == -1 || s2_idx == -1)
     {
-        return count;
+        return 0;
+    }
+
+    // std::cout << " handling " << s1.substr(0, s1_idx+1) << " " << s2.substr(0, s2_idx+1) << '\n';
+    
+    if(results[s1_idx][s2_idx] != -1)
+    {
+        // std::cout << s1.substr(0, s1_idx+1) << " " << s2.substr(0, s2_idx+1) 
+        //           << " seen before with result " << results[s1_idx][s2_idx] << '\n';
+        
+        return results[s1_idx][s2_idx];
     }
 
     if(s1[s1_idx] == s2[s2_idx])
     {
-        return lcs(s1, s1_idx - 1, s2, s2_idx - 1, count + 1);
+        results[s1_idx][s2_idx] = 1 + lcs(s1, s1_idx - 1, s2, s2_idx - 1, results);
+        return results[s1_idx][s2_idx];
     }
 
-    count = std::max({count, 
-        lcs(s1, s1_idx - 1, s2, s2_idx, count), 
-        lcs(s1, s1_idx, s2, s2_idx - 1, count)}); 
-    return count;
+    auto count = std::max({lcs(s1, s1_idx, s2, s2_idx - 1, results), 
+                           lcs(s1, s1_idx - 1, s2, s2_idx, results)}); 
+
+    results[s1_idx][s2_idx] = count;
+    
+    return results[s1_idx][s2_idx];
 }
 
 int commonChild(std::string s1, std::string s2) 
 {
     int result = 0;
+    std::vector<std::vector<int>> cached_results{s1.size()};
+    for(int i = 0; i < s1.size(); ++i)
+    {
+        // -1 means uninitialized
+        cached_results[i] = std::vector<int>(s2.size(), -1);
+    }
 
-    result = lcs(s1, s1.size() - 1, s2, s2.size() - 1, 0);
+    result = lcs(s1, s1.size() - 1, s2, s2.size() - 1, cached_results);
 
     return result;
 }
