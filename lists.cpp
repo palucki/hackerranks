e1d62e1520946f9c12209e3ee1657b59cd65fa5a
palucki <piotrek.palucki@gmail.com>
2023-10-27 19:56:30 +0200

    Completed has_cycle

--- lists.cpp
+++ lists.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <map>
 #include <cmath>
+#include <unordered_set>
 
 struct SinglyLinkedListNode {
     int data;
@@ -44,21 +45,56 @@ void printList(SinglyLinkedListNode* head)
     }
 }
 
+struct Node 
+{
+    int data;
+    struct Node* next;
+};
+
+bool has_cycle(Node* head) 
+{
+    std::unordered_set<Node*> visited;
+    while(head)
+    {
+        if(visited.find(head) != visited.end())
+        {
+            return true;
+        }
+
+        visited.insert(head);
+        head = head->next;
+    }
+
+    return false;
+}
+
 int main()
 {
-    SinglyLinkedListNode head{0, nullptr};
+    // SinglyLinkedListNode head{0, nullptr};
 
-    SinglyLinkedListNode *it = &head;
+    // SinglyLinkedListNode *it = &head;
 
-    it = insertNodeAtPosition(it, 16, 1);
-    it = insertNodeAtPosition(it, 13, 1);
-    it = insertNodeAtPosition(it, 7, 2);
+    // it = insertNodeAtPosition(it, 16, 1);
+    // it = insertNodeAtPosition(it, 13, 1);
+    // it = insertNodeAtPosition(it, 7, 2);
 
-    printList(it);
+    // printList(it);
 
     // insertNodeAtPosition(&head, 1, 2);
 
     // printList(&head);
+
+    Node head{0, nullptr};
+    Node n1{1, nullptr};
+    Node n2{2, nullptr};
+    Node n3{3, nullptr};
+
+    head.next = &n1;
+    n1.next = &n2;
+    n2.next = &n3;
+    n3.next = &head;
+
+    std::cout << has_cycle(&head) << '\n';
 
     return 0;
 }
