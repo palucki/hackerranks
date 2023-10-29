e180504a7f0b3aae26932f6999f349c5f37a7461
palucki <piotrek.palucki@gmail.com>
2023-10-29 16:19:20 +0100

    Completed findMergeNode

--- lists.cpp
+++ lists.cpp
@@ -89,6 +89,76 @@ DoublyLinkedListNode* reverse(DoublyLinkedListNode* llist)
     return new_head;
 }
 
+DoublyLinkedListNode* sortedInsert(DoublyLinkedListNode* llist, int data) 
+{
+    // std::cout << "inserting " << data << '\n';
+    auto new_node = new DoublyLinkedListNode{data};
+    new_node->next = nullptr;
+    new_node->prev = nullptr;
+    auto* head = llist;
+
+    if(llist == nullptr)
+    {
+        new_node->next = llist;
+        head = new_node;
+        return head;
+    }
+
+    DoublyLinkedListNode* prev = llist;
+    while(llist && llist->data < data)
+    {
+        // std::cout << "at " << llist->data << '\n';
+        prev = llist;
+        llist = llist->next;
+    }
+
+    //llist points to location where to insert
+    if(!llist)
+    {
+        // std::cout << "Insert at the end\n";
+        prev->next = new_node;
+        new_node->prev = prev;
+    }
+    else if(!llist->prev)
+    {
+        // std::cout << "Insert at the begining\n";
+        new_node->next = llist;
+        llist->prev = new_node;
+        head = new_node;
+    }
+    else
+    {
+        // std::cout << "Insert in the middle\n";
+        prev->next = new_node;
+        llist->prev = new_node;
+        new_node->next = llist;
+        new_node->prev = prev;
+    }
+
+    return head;
+}
+
+int findMergeNode(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) 
+{
+    std::set<SinglyLinkedListNode*> list1;
+
+    while(head1)
+    {
+        list1.insert(head1);
+        head1 = head1->next;
+    }
+
+    while(head2)
+    {
+        if(list1.find(head2) != list1.end())
+        {
+            return head2->data;
+        }
+        head2 = head2->next;
+    }
+
+    return -1;
+}
 
 int main()
 {
@@ -106,25 +176,42 @@ int main()
 
     // printList(&head);
 
-    DoublyLinkedListNode head{0, nullptr, nullptr};
-    DoublyLinkedListNode n1{1, nullptr, nullptr};
-    DoublyLinkedListNode n2{2, nullptr, nullptr};
-    DoublyLinkedListNode n3{3, nullptr, nullptr};
+    // DoublyLinkedListNode head{1, nullptr, nullptr};
+    // DoublyLinkedListNode n1{2, nullptr, nullptr};
+    // DoublyLinkedListNode n2{5, nullptr, nullptr};
 
-    head.next = &n1;
-    n1.next = &n2;
-    n1.prev = &head;
+    // head.next = &n1;
+    // n1.next = &n2;
+    // n1.prev = &head;
+    // n2.prev = &n1;
+
+    // auto* it = &head;
+
+    // printList(it);
+
+    // it = sortedInsert(it, 0);
+    // it = sortedInsert(it, 3);
+    // it = sortedInsert(it, 6);
+
+    // std::cout << "after\n";
+    // printList(it);
+
+    SinglyLinkedListNode head{1, nullptr};
+    SinglyLinkedListNode n2{2, nullptr};
+    SinglyLinkedListNode n3{3, nullptr};
+    SinglyLinkedListNode n4{4, nullptr};
+
+    SinglyLinkedListNode n11{1, nullptr};
+    SinglyLinkedListNode n12{2, nullptr};
+
+    head.next = &n2;
     n2.next = &n3;
-    n2.prev = &n1;
-    n3.prev = &n2;
-    // n3.next = &head;
+    n3.next = &n4;
 
-    auto* it = &head;
+    n11.next = &n12;
+    n12.next = &n3;
 
-    printList(it);
-    it = reverse(it);
-    std::cout << "after\n";
-    printList(it);
+    std::cout << "Merge point " << findMergeNode(&head, &n11) << '\n';
 
     return 0;
 }
